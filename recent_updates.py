7e06cc3bdbde2d4f629c4c1dd5243e0c5ac605c7
visayafan <visayafan@gmail.com>
2012-11-20 00:26:18 +0800

    Elisp.html Emacs.html修改

--- recent_updates.py
+++ recent_updates.py
@@ -6,7 +6,7 @@ call('cd ~/visayafan.github.com', shell=True)
 call('git blame index.html > shan.html', shell=True)
 content = open('shan.html', 'r').read()
 call('rm -rf shan.html', shell=True)
-dateAndLink = re.compile('(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}):\d{2}.*\<li\>\<a(\shref="(?=Linux|Coding|Algorithm|Compiler|Others|English).*\>)(.+)(\</a\>\</li\>)')
+dateAndLink = re.compile('(\d{2}-\d{2}-\d{2}\s\d{2}:\d{2}):\d{2}.*\<li\>\<a(\shref="(?=Linux|Coding|Algorithm|Compiler|Others|English).*\>)(.+)(\</a\>\</li\>)')
 allCont = dateAndLink.findall(content)
 allCont.sort()
 # for vdate, vlinkpre, vname, vlinkafter in allCont:
